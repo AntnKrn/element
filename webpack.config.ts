1c3c471776306a0ea66d9a66af938b0eec96d3b2
Anton Kuryan <super.tosh2016@yandex.by>
2024-07-10 17:45:25 +0300

    feat: add webpack

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import webpack from "webpack";
+import { buildWebpack } from "./config/build/buildWebpack";
+import { buildMode, buildPaths } from "./config/build/types/types";
+import path from "path";
+
+type envVariables = {
+  mode: buildMode;
+  port: number;
+};
+
+export default (env: envVariables) => {
+  const paths: buildPaths = {
+    output: path.resolve(__dirname, "build"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    public: path.resolve(__dirname, "public"),
+    src: path.resolve(__dirname, "src"),
+  };
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths,
+  });
+
+  return config;
+};
