1404acc6e46a0ee68f79f9d51b74d7bb75fe7806
Anton Kuryan <super.tosh2016@yandex.by>
2024-07-10 21:46:41 +0300

    feat: add eslint config

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./components/App";
+import { createRoot } from 'react-dom/client';
 
-const root = document.getElementById("root");
+import { App } from './components/App';
+
+const root = document.getElementById('root');
 
 if (!root) {
-  throw new Error("root not found");
+  throw new Error('root not found');
 }
 createRoot(root).render(<App />);
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,8 @@
-import webpack from "webpack";
-import { buildWebpack } from "./config/build/buildWebpack";
-import { buildMode, buildPaths } from "./config/build/types/types";
-import path from "path";
+import path from 'path';
+import webpack from 'webpack';
+
+import { buildWebpack } from './config/build/buildWebpack';
+import { buildMode, buildPaths } from './config/build/types/types';
 
 type envVariables = {
   mode: buildMode;
@@ -10,15 +11,15 @@ type envVariables = {
 
 export default (env: envVariables) => {
   const paths: buildPaths = {
-    output: path.resolve(__dirname, "build"),
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    html: path.resolve(__dirname, "public", "index.html"),
-    public: path.resolve(__dirname, "public"),
-    src: path.resolve(__dirname, "src"),
+    output: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
+    src: path.resolve(__dirname, 'src'),
   };
   const config: webpack.Configuration = buildWebpack({
     port: env.port ?? 3000,
-    mode: env.mode ?? "development",
+    mode: env.mode ?? 'development',
     paths,
   });
 
