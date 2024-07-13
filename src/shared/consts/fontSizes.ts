c11c6577ffe536440d36335494ef24d083339c13
Anton Kuryan <super.tosh2016@yandex.by>
2024-07-13 17:07:31 +0300

    add fonts

---
+++ src/shared/consts/fontSizes.ts
@@ -0,0 +1,49 @@
+const REGULAR = 400;
+const MEDIUM = 500;
+const BOLD = 700;
+
+class TypeFace {
+  Weight: number;
+  Size: string;
+  LineHeight: string;
+  ParagraphSpacing: string;
+
+  constructor(
+    Weight: number,
+    Size: string,
+    LineHeight: string,
+    ParagraphSpacing: string,
+  ) {
+    this.Weight = Weight;
+    this.Size = Size;
+    this.LineHeight = LineHeight;
+    this.ParagraphSpacing = ParagraphSpacing;
+  }
+}
+
+export const DESKTOP_HEADING1 = new TypeFace(MEDIUM, '33px', 'none', 'none');
+export const DESKTOP_HEADING2 = new TypeFace(REGULAR, '26px', '35px', 'none');
+export const DESKTOP_HEADING3 = new TypeFace(REGULAR, '20px', '26px', 'none');
+export const DESKTOP_HEADING4 = new TypeFace(MEDIUM, '20px', '20px', 'none');
+export const DESKTOP_HEADING5 = new TypeFace(REGULAR, '16px', '27px', '10px');
+export const DESKTOP_BODY_LARGE = new TypeFace(BOLD, '16px', 'none', 'none');
+export const DESKTOP_BODY_MEDIUM = new TypeFace(
+  REGULAR,
+  '14px',
+  'none',
+  'none',
+);
+export const DESKTOP_BODY_SMALL = new TypeFace(REGULAR, '12px', '20px', '10px');
+
+export const MOBILE_HEADING1 = new TypeFace(REGULAR, '20px', '27px', 'none');
+export const MOBILE_HEADING2 = new TypeFace(REGULAR, '16px', '27px', '10px');
+export const MOBILE_HEADING3 = new TypeFace(REGULAR, '14px', 'none', 'none');
+export const MOBILE_BODY_SMALL = new TypeFace(REGULAR, '12px', '20px', '10px');
+
+export const fdsafasd = `
+    font-family: DM Sans;
+    font-size: 33px;
+    font-weight: 500;
+    line-height: 43px;
+    text-align: left;
+`;
